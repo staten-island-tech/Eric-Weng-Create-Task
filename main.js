792e6a6e152938f85c59650bd2860b88f816baee
Ericweng2023 <95102206+Ericweng2023@users.noreply.github.com>
2022-09-16 21:18:49 -0400

    dqwdqwd

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 const URL = "https://periodic-table-elements-info.herokuapp.com/elements";
 
-getData(URL);
-
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data[0].symbol);
-
-    function displayE(data) {
-      DOMSelectors.display.insertAdjacentHTML(
-        "afterbegin",
-        `<div class="display-card">
-<h3 class="display-text" >${data.symbol}</h2>
-      </div>
- `
-      );
-    }
-    displayE();
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 const DOMSelectors = {
   box: document.getElementById("box"),
   display: document.getElementById("display"),
+  start: document.getElementById("start"),
+  skip: document.getElementById("skip"),
 };
+
+DOMSelectors.start.addEventListener("click", function (e) {
+  e.preventDefault();
+  DOMSelectors.start.style.display = "none";
+  getData(URL);
+
+  async function getData(URL) {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+
+      function shuffle(data) {
+        data.sort(() => Math.random() - 0.5);
+      }
+      shuffle(data);
+      console.log(data[0]);
+
+      function displayE() {
+        DOMSelectors.display.insertAdjacentHTML(
+          "afterbegin",
+          `<div class="display-card">
+<h3 class="display-text" >${data[0].symbol}</h2>
+      </div>
+      <form id="form">
+      <label for="fname">Your Response</label>
+      <input type="text" required id="fname" name="fname">
+    </form>
+
+  <button id="skip">Skip</button>
+ `
+        );
+      }
+      displayE();
+
+      skip.addEventListener("click", () => data[id + 1]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
