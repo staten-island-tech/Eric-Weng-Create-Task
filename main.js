6876f91e48d725242cd066f3863aa004dc772020
Ericweng2023 <95102206+Ericweng2023@users.noreply.github.com>
2022-09-18 01:48:25 -0400

    cwcw

--- main.js
+++ main.js
@@ -5,44 +5,57 @@ const DOMSelectors = {
   display: document.getElementById("display"),
   start: document.getElementById("start"),
   skip: document.getElementById("skip"),
+  form: document.getElementById("form"),
+  name: document.getElementById("name"),
+  card: document.getElementById("display-card"),
 };
 
-DOMSelectors.start.addEventListener("click", function (e) {
-  e.preventDefault();
-  DOMSelectors.start.style.display = "none";
-  getData(URL);
+getData(URL);
 
-  async function getData(URL) {
-    try {
-      const response = await fetch(URL);
-      const data = await response.json();
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
 
-      function shuffle(data) {
-        data.sort(() => Math.random() - 0.5);
-      }
-      shuffle(data);
-      console.log(data[0]);
+    function shuffle(data) {
+      data.sort(() => Math.random() - 0.5);
+    }
+    shuffle(data);
+    console.log(data[0]);
 
-      function displayE() {
-        DOMSelectors.display.insertAdjacentHTML(
-          "afterbegin",
-          `<div class="display-card">
+    function hide() {
+      DOMSelectors.card.hide();
+    }
+
+    function displayE() {
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="display-card">
 <h3 class="display-text" >${data[0].symbol}</h2>
       </div>
-      <form id="form">
-      <label for="fname">Your Response</label>
-      <input type="text" required id="fname" name="fname">
-    </form>
-
-  <button id="skip">Skip</button>
  `
-        );
-      }
-      displayE();
-
-      skip.addEventListener("click", () => data[id + 1]);
-    } catch (err) {
-      console.error(err);
+      );
+      DOMSelectors.form.addEventListener("submit", function (e) {
+        if (DOMSelectors.name.value === data[0].name) {
+          hide();
+          e.preventDefault();
+          shuffle(data);
+          displayE();
+          DOMSelectors.form.reset();
+          console.log(data[0]);
+          console.log(DOMSelectors.name.value);
+        }
+      });
     }
+    displayE();
+    DOMSelectors.skip.addEventListener("click", hide()){
+      shuffle(data);
+      displayE();
+      e.preventDefault();
+      DOMSelectors.form.reset();
+      console.log(data[0]);
+    }
+  } catch (err) {
+    console.error(err);
   }
-});
+}
