366f044737bb4ba80443f748359d475a4aa25523
Ericweng2023 <95102206+Ericweng2023@users.noreply.github.com>
2022-09-19 17:45:13 -0400

    bbbb

--- main.js
+++ main.js
@@ -16,59 +16,69 @@ async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
+    var QuestionIndex = 0;
 
-    function shuffle(data) {
-      data.sort(() => Math.random() - 0.5);
-    }
+    const shuffle = data
+      .sort(() => Math.random() - 0.5)
+      .map((data) => ({
+        name: data.name,
+        symbol: data.symbol,
+      }));
 
-    const newquestion = data.map((data, index) => ({
-      name: data.name,
-      symbol: data.symbol,
-      id: index + 1,
-    }));
-
-    console.log(newquestion);
-
-    shuffle(data);
-    console.log(data[0]);
+    console.log(shuffle);
 
     function displayE() {
       DOMSelectors.card.insertAdjacentHTML(
         "afterbegin",
         `<div class="card">
-<h3 class="display-text" >${data[0].symbol}</h2>
+<h3 class="display-text" >${shuffle[QuestionIndex].symbol}</h2>
       </div>
  `
+      );
+    }
+    function displayMSG() {
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <div class="message">
+        <h2 class="display-text"><That is the LAST element!!!!!</h2>
+        </div>
+      
+      `
       );
     }
     DOMSelectors.form.addEventListener("submit", function (e) {
       e.preventDefault();
       const answer = DOMSelectors.name.value;
-      const correct = data[0].name;
+      const correct = shuffle[QuestionIndex].name;
+
       if (answer.toLowerCase() === correct.toLowerCase()) {
+        QuestionIndex = QuestionIndex + 1;
+        console.log(QuestionIndex);
         DOMSelectors.card.innerHTML = "";
-        displayE(data[id + 1].symbol);
+        displayE(shuffle[QuestionIndex].symbol);
         DOMSelectors.form.reset();
-        console.log(data[0]);
-      }
-      if (shuffle(data[0]) === data[0]) {
-        shuffle(data);
-        displayE();
-        DOMSelectors.form.reset();
-        console.log(data[0]);
       }
       if (answer.toLowerCase() !== correct.toLowerCase()) {
         DOMSelectors.form.reset();
+      }
+      if (QuestionIndex > 118) {
+        alert("That is the LAST element!!!!!");
+        return false;
       }
     });
     displayE();
     DOMSelectors.skip.addEventListener("click", function (e) {
       e.preventDefault();
+      QuestionIndex = QuestionIndex + 1;
       DOMSelectors.card.innerHTML = "";
-      shuffle(data);
-      displayE();
+      displayE(shuffle[QuestionIndex].symbol);
       DOMSelectors.form.reset();
-      console.log(data[0]);
+      if (QuestionIndex > 118) {
+        displayMSG();
+        alert("That is the LAST element!!!!!");
+        return false;
+      }
     });
   } catch (err) {
     console.error(err);
